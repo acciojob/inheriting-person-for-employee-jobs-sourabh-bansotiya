aa888e15dbd4448fd3dbd63de4ba14b41286b305
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-21 16:58:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ class Employee extends Person {
 		this.jobTitle = jobTitle;
 	}
 	jobGreet(){
-		console.log(`Hello, my name is ${this.name} I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
 	}
 }
 
